8ff4eff022e960afcb989670dcf8c9a89489c36c
psawicki0 <01143606@pw.edu.pl>
2019-05-11 16:36:39 +0200

    0.2

--- proi3.cpp
+++ proi3.cpp
@@ -4,57 +4,68 @@
 #include <vector>
 enum ClientType {individual, business};
 class Account{
-    unsigned int id;
+    int id;
     ClientType type;
     long long balance;
 
     public:
-    Account(const unsigned int &aid) : id(aid){
+    Account(int aid = -1) : id(aid){
         type = ClientType::individual; //todo: randomize
         balance = 0; //todo: randomize
     }
-    unsigned int getID() const{
+    int getID() const{
 	    return id;
     }
 };
 
 class BankElement{
 	protected:
-    unsigned int id;
+    int id;
     std::queue<std::tuple<Account, unsigned int>> queue;
     unsigned int timeRemaining;
 
     public:
-    BankElement(const unsigned int &bid) : id(bid),  {}
+    BankElement(int bid) : id(bid), queue(), timeRemaining(0) {}
     virtual void getInfo(const Account &client) = 0;
+    int getID() const{
+	    return id;
+    }
+    void showQueue() const{
+	    std::cout << std::get<0>(queue.front()).getID() << "\t" << std::get<1>(queue.front()) << std::endl;
+	    std::cout << "remaining: " << timeRemaining << std::endl;
+    }
 };
 
 class Teller : public BankElement{
     public:
-    Teller(const unsigned int &tid) : BankElement(tid){}
+    Teller(int tid = -1) : BankElement(tid){}
     virtual void getInfo(const Account &client){
         queue.push({client, 5});
+	if(!timeRemaining)
+		timeRemaining = 5;
         std::cout << "Client " << client.getID() << "joins queue to teller " << id << std::endl;
     }
 };
 
 class ATM : public BankElement{
     public:
-    ATM(const unsigned int &aid) : BankElement(aid){}
+    ATM(int aid = -1) : BankElement(aid){}
     virtual void getInfo(const Account &client){
         queue.push({client, 3});
+	if(!timeRemaining)
+		timeRemaining = 3;
         std::cout << "Client " << client.getID() << "joins queue to ATM " << id << std::endl;
     }
 };
 
 class InputTM : public ATM{
     public:
-    InputTM(const unsigned int &iid) : ATM(iid){}
+    InputTM(int iid = -1) : ATM(iid){}
 };
 
 class OutputTM : public ATM{
     public:
-    OutputTM(const unsigned int &oid) : ATM(oid){}
+    OutputTM(int oid = -1) : ATM(oid){}
 };
 
 class BankBranch{
@@ -65,10 +76,10 @@ class BankBranch{
     OutputTM otm;
 
     BankBranch(const unsigned int &clientsAmount, const unsigned int &tellersAmount)
-    : itm(InputTM(tellersAmount)), otm(OutputTM(tellersAmount + 1)), clients(std::vector<Account>(clientsAmount)),
-		    tellers(std::vector<Teller>(tellersAmount)){
+    : itm(InputTM(tellersAmount)), otm(OutputTM(tellersAmount + 1)), clients(std::vector<Account>(0)),
+		    tellers(std::vector<Teller>(0)){
         unsigned int max = clientsAmount > tellersAmount ? clientsAmount : tellersAmount;
-        unsigned int i = 0;
+        int i = 0;
         for(; i < max; ++i){
             if(i < clientsAmount)
                 clients.push_back(Account(i));
@@ -79,5 +90,7 @@ class BankBranch{
 };
 int main(){
     BankBranch bb(5, 5);
-    bb.tellers[0].getInfo(bb.clients[0]);
+    BankElement *elem = &bb.otm;
+    elem->getInfo(bb.clients[0]);
+    elem->showQueue();
 }
