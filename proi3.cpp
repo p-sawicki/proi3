82f1455798793181c877e83325dbec278da9bd5d
psawicki0 <01143606@pw.edu.pl>
2019-05-11 17:44:22 +0200

    0.3

--- proi3.cpp
+++ proi3.cpp
@@ -2,19 +2,25 @@
 #include <queue>
 #include <tuple>
 #include <vector>
+#include <string>
 enum ClientType {individual, business};
+enum ClientState {notBusy, busy, loanEval};
 class Account{
     int id;
     ClientType type;
+    ClientState state;
     long long balance;
 
     public:
-    Account(int aid = -1) : id(aid){
-        type = ClientType::individual; //todo: randomize
-        balance = 0; //todo: randomize
+    Account(int aid = -1) : id(aid), state(ClientState::notBusy){
+        type = ClientType::individual; //TODO: randomize
+        balance = 0; //TODO: randomize
     }
     int getID() const{
 	    return id;
+    }
+    void setState(const ClientState &s){
+	    state = s;
     }
 };
 
@@ -23,10 +29,14 @@ class BankElement{
     int id;
     std::queue<std::tuple<Account, unsigned int>> queue;
     unsigned int timeRemaining;
+    std::string name;
 
     public:
-    BankElement(int bid) : id(bid), queue(), timeRemaining(0) {}
-    virtual void getInfo(const Account &client) = 0;
+    BankElement(int bid, std::string n) : id(bid), queue(), timeRemaining(0), name(n) {}
+    virtual void getInfo(Account &client) = 0;
+    virtual void changePIN(Account &client) = 0;
+    virtual void takeMoney(Account &client) = 0;
+    virtual void depositMoney(Account &client) = 0;
     int getID() const{
 	    return id;
     }
@@ -34,38 +44,66 @@ class BankElement{
 	    std::cout << std::get<0>(queue.front()).getID() << "\t" << std::get<1>(queue.front()) << std::endl;
 	    std::cout << "remaining: " << timeRemaining << std::endl;
     }
+    void add(Account &client, const unsigned int &time, const std::string &nname, ClientState s = ClientState::busy){
+	    queue.push({client, time});
+	    if(!timeRemaining)
+		    timeRemaining = time;
+	    client.setState(s);
+	    std::cout << "Client " << client.getID() << " joins queue to " << nname << id << std::endl;
+    }
 };
 
 class Teller : public BankElement{
     public:
-    Teller(int tid = -1) : BankElement(tid){}
-    virtual void getInfo(const Account &client){
-        queue.push({client, 5});
-	if(!timeRemaining)
-		timeRemaining = 5;
-        std::cout << "Client " << client.getID() << "joins queue to teller " << id << std::endl;
+    Teller(int tid = -1) : BankElement(tid, "Teller ") {}
+    virtual void getInfo(Account &client){
+	add(client, 5, name);
+    }
+    virtual void changePIN(Account &client){
+	    add(client, 6, name);
+    }
+    virtual void takeMoney(Account &client){
+	    add(client, 7, name);
+    }
+    virtual void depositMoney(Account &client){
+	    add(client, 7, name);
+    }
+    void takeLoan(Account &client){
+	    add(client, 15, name, ClientState::loanEval);
     }
 };
 
 class ATM : public BankElement{
     public:
-    ATM(int aid = -1) : BankElement(aid){}
-    virtual void getInfo(const Account &client){
-        queue.push({client, 3});
-	if(!timeRemaining)
-		timeRemaining = 3;
-        std::cout << "Client " << client.getID() << "joins queue to ATM " << id << std::endl;
+    ATM(int aid = -1) : BankElement(aid, "ATM ") {}
+    virtual void getInfo(Account &client){
+	    add(client, 3, name);
+    }
+    virtual void changePIN(Account &client){
+	    add(client, 4, name);
     }
 };
 
 class InputTM : public ATM{
     public:
     InputTM(int iid = -1) : ATM(iid){}
+    virtual void takeMoney(Account &client){
+	    std::cout << "can't do that here!\n"; //TODO: exception
+    }
+    virtual void depositMoney(Account &client){
+	    add(client, 5, name);
+    }
 };
 
 class OutputTM : public ATM{
     public:
     OutputTM(int oid = -1) : ATM(oid){}
+    virtual void takeMoney(Account &client){
+	    add(client, 5, name);
+    }
+    virtual void depositMoney(Account &client){
+	    std::cout << "can't do that here!\n"; //TODO: exception
+    }
 };
 
 class BankBranch{
@@ -92,5 +130,6 @@ int main(){
     BankBranch bb(5, 5);
     BankElement *elem = &bb.otm;
     elem->getInfo(bb.clients[0]);
+    elem->changePIN(bb.clients[1]);
     elem->showQueue();
 }
