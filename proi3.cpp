3fc1d7cbc46a368bc3534f81a3e412f5557fcbfc
psawicki0 <01143606@pw.edu.pl>
2019-05-12 16:40:11 +0200

    0.4

--- proi3.cpp
+++ proi3.cpp
@@ -3,6 +3,9 @@
 #include <tuple>
 #include <vector>
 #include <string>
+#include <chrono>
+#include <random>
+#include <thread>
 enum ClientType {individual, business};
 enum ClientState {notBusy, busy, loanEval};
 class Account{
@@ -43,6 +46,9 @@ class BankElement{
     void showQueue() const{
 	    std::cout << std::get<0>(queue.front()).getID() << "\t" << std::get<1>(queue.front()) << std::endl;
 	    std::cout << "remaining: " << timeRemaining << std::endl;
+    }
+    size_t getQueueSize() const{
+	    return queue.size();
     }
     void add(Account &client, const unsigned int &time, const std::string &nname, ClientState s = ClientState::busy){
 	    queue.push({client, time});
@@ -125,11 +131,75 @@ class BankBranch{
                 tellers.push_back(Teller(i));
         }
     }
+    BankElement* getShortestQueue(bool includeOTM, bool includeITM){
+	BankElement* ans = &tellers[0];
+	unsigned int shortest = tellers[0].getQueueSize();
+	if(includeOTM){
+		if(!otm.getQueueSize())
+			return &otm;
+		if(otm.getQueueSize() < shortest){
+			shortest = otm.getQueueSize();
+			ans = &otm;
+		}
+	}
+	if(includeITM){
+		if(!itm.getQueueSize())
+			return &itm;
+		if(itm.getQueueSize() < shortest){
+			shortest = itm.getQueueSize();
+			ans = &itm;
+		}
+	}
+	for(int i = 0; i < tellers.size(); ++i){
+		if(!tellers[i].getQueueSize())
+			return &tellers[i];
+		if(i && tellers[i].getQueueSize() < shortest){
+			shortest = tellers[i].getQueueSize();
+			ans = &tellers[i];
+		}
+	}
+	return ans;	
+    }
+    void simulate(){
+	unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::mt19937 gen(seed);
+	std::uniform_int_distribution<unsigned int> chanceClientComes(0, 1);
+	std::uniform_int_distribution<unsigned int> clientIDDistribution(0, clients.size() - 1);
+	std::uniform_int_distribution<unsigned int> clientActionDistribution(0, 4);
+	while(true){
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		if(!chanceClientComes(gen)){
+			std::cout << "No client\n";
+			continue;
+		}
+		unsigned int clientID = clientIDDistribution(gen);
+		Account &chosen = clients[clientID];
+		std::cout << "Client " << clientID << " comes in";
+		unsigned int clientAction = clientActionDistribution(gen);
+		if(clientAction == 0){
+			std::cout << " to get info\n";
+			getShortestQueue(1, 1)->getInfo(chosen);
+		}
+		else if(clientAction == 1){
+			std::cout << " to change PIN\n";
+			getShortestQueue(1, 1)->changePIN(chosen);
+		}
+		else if(clientAction == 2){
+			std::cout << " to take money out\n";
+			getShortestQueue(1, 0)->takeMoney(chosen);
+		}
+		else if(clientAction == 3){
+			std::cout << " to deposit money\n";
+			getShortestQueue(0, 1)->depositMoney(chosen);
+		}
+		else{ 
+			std::cout << " to take out a loan\n";
+			dynamic_cast<Teller*>(getShortestQueue(0, 0))->takeLoan(chosen);
+		}
+	}
+    }
 };
 int main(){
     BankBranch bb(5, 5);
-    BankElement *elem = &bb.otm;
-    elem->getInfo(bb.clients[0]);
-    elem->changePIN(bb.clients[1]);
-    elem->showQueue();
+    bb.simulate();
 }
