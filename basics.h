2fd60c346a26e38113d08e64478368287db9a0d5
psawicki0 <01143606@pw.edu.pl>
2019-05-19 21:00:19 +0200

    1.1

--- basics.h
+++ basics.h
@@ -18,18 +18,18 @@ enum ClientState {notBusy, busy, loanEval};
 template <class T = std::ostream>
 class Logger{
     private:
-    T stream;
+    T *stream;
     public:
-    Logger(T &s) : stream(s) {}
+    Logger(T *s) : stream(s) {}
     void open(std::string name){
-        stream.open(name, std::ios::out, std::ios::trunc);
+        stream->open(name, std::ios::out, std::ios::trunc);
     }
     T getStream() const{
-        return stream;
+        return *stream;
     }
     void write(std::string message){
-        stream << message;
-        if(!stream){
+        *stream << message;
+        if(!*stream){
             std::string message("ERR2 - An error with the log file occurred. Try restarting the program.\n");
             std::runtime_error err(message);
             throw err;
@@ -43,13 +43,13 @@ inline std::mt19937& gen(){
 }
 inline Logger<std::fstream>& file(){
     static std::fstream stream;
-    static Logger<std::fstream> logFile(stream);
+    static Logger<std::fstream> logFile(&stream);
     if(!logFile.getStream().is_open())
         logFile.open("log.txt");
     return logFile;
 }
 inline Logger<>& console(){
-    static Logger<> logConsole(std::cout);
+    static Logger<> logConsole(&std::cout);
     return logConsole;
 }
 inline void logBoth(const std::string &message){
