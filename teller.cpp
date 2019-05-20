e8b16a682d767d209b4c043e8b4e12a056e1eec5
psawicki0 <01143606@pw.edu.pl>
2019-05-20 11:48:03 +0200

    1.5

--- teller.cpp
+++ teller.cpp
@@ -1,25 +1,25 @@
 //author: Piotr Sawicki
 #include "teller.h"
 void Teller::evalLoan(long long &branchBalance){
-	std::uniform_int_distribution<unsigned int> chancePositiveDecision(0, 2);
-	unsigned int decision = chancePositiveDecision(gen());
-	Account &client = std::get<0>(queue.front());
-	if(decision == 2 && client.getBalance() >= 0){
-		const long long maxLoanAmount = 1'000;
-		const long long loanAmountMultiplier = 1'000;
-	    std::uniform_int_distribution<long long> loanAmountDistribution(1, maxLoanAmount);
-	    long long loan = loanAmountDistribution(gen()) * loanAmountMultiplier;
-		client += loan;
-    	branchBalance -= loan;
-		std::stringstream message;
-    	message << "Client " << client.getID() << " was approved for a loan of $" << loan << ".";
-    	logBoth(message.str());
-    	newBalance(client);
+    std::uniform_int_distribution<unsigned int> chancePositiveDecision(0, 2);
+    unsigned int decision = chancePositiveDecision(gen());
+    Account &client = std::get<0>(queue.front());
+    if(decision == 2 && client.getBalance() >= 0){
+        const long long maxLoanAmount = 1'000;
+        const long long loanAmountMultiplier = 1'000;
+        std::uniform_int_distribution<long long> loanAmountDistribution(1, maxLoanAmount);
+        long long loan = loanAmountDistribution(gen()) * loanAmountMultiplier;
+        client += loan;
+        branchBalance -= loan;
+        std::stringstream message;
+        message << "Client " << client.getID() << " was approved for a loan of $" << loan << ".";
+        logBoth(message.str());
+        newBalance(client);
     }
     else{
-    	std::stringstream message;
-    	message << "Client " << client.getID() << " was not approved for a loan.\n";
-    	logBoth(message.str());
+        std::stringstream message;
+        message << "Client " << client.getID() << " was not approved for a loan.\n";
+        logBoth(message.str());
     }
 }
 Teller::Teller(int tid) : BankElement(tid, "Teller "){}
@@ -39,8 +39,8 @@ void Teller::withdrawMoney(Account &client, long long &branchBalance){
 	withdraw(client, branchBalance);
 }
 void Teller::depositMoney(Account &client, long long &branchBalance){
-	const unsigned int timeToDepositMoneyTeller = 7;
-	add(client, timeToDepositMoneyTeller);
+    const unsigned int timeToDepositMoneyTeller = 7;
+    add(client, timeToDepositMoneyTeller);
     deposit(client, branchBalance);
 }
 void Teller::takeLoan(Account &client){
@@ -51,26 +51,26 @@ void Teller::takeLoan(Account &client){
 	logBoth(message.str());
 }
 void Teller::simulate(long long &branchBalance){
-	if(timeRemaining)
-    	--timeRemaining;
+    if(timeRemaining)
+        --timeRemaining;
     else
-    	return;
+        return;
     if(!timeRemaining){
-    	if(std::get<0>(queue.front()).getState() == ClientState::loanEval){
-    		std::get<0>(queue.front()).setState(ClientState::busy);
-    		const unsigned int timeToSignLoan = 10;
-    		timeRemaining = timeToSignLoan;
-    		evalLoan(branchBalance);
-    	}
-    	else{
-    		std::get<0>(queue.front()).setState(ClientState::notBusy);
-    		queue.pop();
-    		if(!queue.empty()){
-    		    timeRemaining = std::get<1>(queue.front());
-				std::stringstream message;
-    	    	message << "Client " << std::get<0>(queue.front()).getID() << " is now in front of queue to " << name << id << "\n";
-				console().write(message.str());
-    		}
-    	}
+        if(std::get<0>(queue.front()).getState() == ClientState::loanEval){
+            std::get<0>(queue.front()).setState(ClientState::busy);
+            const unsigned int timeToSignLoan = 10;
+            timeRemaining = timeToSignLoan;
+            evalLoan(branchBalance);
+        }
+        else{
+            std::get<0>(queue.front()).setState(ClientState::notBusy);
+            queue.pop();
+            if(!queue.empty()){
+                timeRemaining = std::get<1>(queue.front());
+                std::stringstream message;
+                message << "Client " << std::get<0>(queue.front()).getID() << " is now in front of queue to " << name << id << "\n";
+                console().write(message.str());
+            }
+        }
     }
 }
