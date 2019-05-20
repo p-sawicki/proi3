3d90f223cb0b750ad74c458a9cfc0e6927eae401
psawicki0 <01143606@pw.edu.pl>
2019-05-20 11:44:26 +0200

    1.5

--- proi3.cpp
+++ proi3.cpp
@@ -10,12 +10,12 @@ int main(int argc, char **argv){
     t.tellerMethodsTest();
     t.branchBankruptExceptionCatchTest();
 #endif
-	std::stringstream conv;
-	for(int i = 1; i < argc; ++i)
-		conv << argv[i] << "\n";
-	unsigned int clientsAmount = 100;
-	unsigned int tellersAmount = 5;
-	unsigned int duration = 60;
+    std::stringstream conv;
+    for(int i = 1; i < argc; ++i)
+        conv << argv[i] << "\n";
+    unsigned int clientsAmount = 100;
+    unsigned int tellersAmount = 5;
+    unsigned int duration = 60;
     if(conv.str()[0] < '0' || conv.str()[0] > '9'){
         std::fstream file;
         std::string fileName;
@@ -24,24 +24,23 @@ int main(int argc, char **argv){
         file >> clientsAmount >> tellersAmount >> duration;
     }
     else
-	    conv >> clientsAmount >> tellersAmount >> duration;
-	BankBranch bb(clientsAmount, tellersAmount, duration);
-	bool simComplete = false;
-	while(!simComplete){
-		try{
-			simComplete = bb.simulate();
-		}
-		catch(std::runtime_error err){
+        conv >> clientsAmount >> tellersAmount >> duration;
+    BankBranch bb(clientsAmount, tellersAmount, duration);
+    bool simComplete = false;
+    while(!simComplete){
+        try{
+            simComplete = bb.simulate();
+        }
+        catch(std::runtime_error err){
             if(err.what()[3] == '1'){
-			    std::stringstream message;
-			    const long long govtLoan = 10'000'000;
-			    message << err.what() << "Depositing government bailout of $" << govtLoan - bb.getBalance() <<" into the bank branch. Reopening branch.\n";
-			    bb.setBalance(govtLoan);
-			    logBoth(message.str());
-	    	}
+                std::stringstream message;
+                const long long govtLoan = 10'000'000;
+                message << err.what() << "Depositing government bailout of $" << govtLoan - bb.getBalance() <<" into the bank branch. Reopening branch.\n";
+                bb.setBalance(govtLoan);
+                logBoth(message.str());
+            }
             else 
                 std::cout << err.what();
-            
         }
         catch(std::bad_alloc err){
             logBoth("ERR4 - Error due to memory allocation. Try restarting the program.\n");
